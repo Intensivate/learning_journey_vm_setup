4210258323760c3befbbc22708c3a28d24140104
Aleksandar Pajkanovic <aleksandar.pajkanovic@gmail.com>
2018-07-05 23:50:08 +0200

    L2 scripts added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# vm_setup
+## Setting-up environment for the L2$ project
+
+### Virtual Machine
+
+Before running the scripts, please first create a Virtual Machine as explained (here)[https://github.com/librecores/riscv-sodor/wiki/Chisel-Environment].
+
+### Running the scripts
+
+Please run the scripts in the following order:
+
+1. `source setup_general.sh`
+1. `source setup_L2_cache.sh`
+
+### Resources
+
+For more details please visit (Chisel Learning Journey)[https://github.com/librecores/riscv-sodor/wiki] and post any questions in (User Experiences)[https://github.com/librecores/riscv-sodor/wiki/User-Experiences] page.
+
---
+++ setup_general.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#This script installs the environment required for all Intensivate projects.
+#It is generally the first thing run after creating a new VM, according to the Intensivate standard
+#For directions on setting up the standard Intensivate VM, see the wiki:
+#https://github.com/librecores/riscv-sodor/wiki/Chisel-Environment
+
+##########################################################
+# Install the needed packages.
+##########################################################
+sudo aptitude --assume-yes install gcc-multilib
+sudo apt-get --assume-yes install git
+sudo apt-get --assume-yes install g++
+sudo apt-get --assume-yes install makeinfo
+sudo apt-get --assume-yes install default-jdk
+sudo apt-get --assume-yes install texinfo
+sudo apt-get --assume-yes install gtkwave
+sudo apt-get --assume-yes install autoconf
+sudo apt-get --assume-yes install scala
+sudo apt-get --assume-yes install libz-dev
+sudo apt-get --assume-yes install automake autotools-dev curl
+sudo apt-get --assume-yes install libmpc-dev libmpfr-dev libgmp-dev
+sudo apt-get --assume-yes install gawk build-essential bison flex gperf
+wget https://dl.bintray.com/sbt/debian/sbt-0.13.12.deb
+sudo dpkg -i sbt-0.13.12.deb
+sudo apt-get update
+sudo apt-get --assume-yes install sbt
+sudo apt-get --assume-yes install libncurses5-dev libusb-1.0-0 libboost-dev swig
+sudo apt-get --assume-yes install libtool libreadline6 libreadline6-dev libelf-dev python-dev xterm
+sudo apt-get --assume-yes install device-tree-compiler patchutils bc zlib1g-dev
+sudo apt-get --assume-yes install libusb-1.0-0-dev
+
+rm -rf sbt-0.13.12.deb
+
+# Replace git:// by https:// to avoid any proxy issues.
+git config --global url."https://".insteadOf git://
